5aa9b93a3dbe4b41e47b2ba2814737e9363bde44
srecabar <recabarrensf@gmail.com>
2016-03-02 18:47:25 -0500

    initial theme files

---
+++ wp-content/themes/codedame/page.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package _tk
+ */
+
+get_header(); ?>
+
+<h1>Code Dame</h1>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
